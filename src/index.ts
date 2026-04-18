6d6e540da8685479e707aeedb0ed33a1f8f817fa
胡博睿 <3599766025@qq.com>
2026-04-18 19:31:41 +0800

    修复参数类型错误导致的宽高调节不生效的问题

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,11 @@ export function about(): void {
 	);
 }
 export async function openScriptTool(): void {
+	const width = await eda.sys_Storage.getExtensionUserConfig('UI_width');
+	const height = await eda.sys_Storage.getExtensionUserConfig('UI_height');
 	eda.sys_Storage.setExtensionUserConfig('version', extensionConfig.version);
-	eda.sys_IFrame.openIFrame('iframe/main/index.html', 1200, 500, 'ScriptTool', { maximizeButton: true, minimizeButton: true });
+	eda.sys_IFrame.openIFrame('iframe/main/index.html', parseInt(width, 10) ?? 1200, parseInt(height, 10) ?? 500, 'ScriptTool', {
+		maximizeButton: true,
+		minimizeButton: true,
+	});
 }
